b85198231ba0b74d954f48854b51eb222dae44d9
Igor Cotrim <igor.santos@verity.com.br>
2022-10-11 09:42:07 -0300

    lifecycle: this, apply, call and arguments

---
+++ 03_javascript-lifecycle-2/01_this-apply-call-arguments/index.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const {
+  watch,
+  promises: { readFile },
+} = require("fs");
+
+class File {
+  watch(event, filename) {
+    console.log("this", this);
+    console.log("arguments", Array.prototype.slice.call(arguments));
+
+    this.showContent(filename);
+  }
+
+  async showContent(filename) {
+    console.log((await readFile(filename)).toString());
+  }
+}
+
+const file = new File();
+// dessa forma, ele ignora o 'this' da classe File herda o this do watch!
+// watch(__filename, file.watch());
+
+// alternativa para nao herdar o this da funcao
+// watch(__filename, (event, filename) => file.watch(event, filename));
+
+// podemos deixar explicito qual é o contexto que a funcao deve seguir
+// o bind retorna uma funcao com o 'this' que se mantem de file, ignorando o watch
+// maneira mais correta!!!
+// watch(__filename, file.watch.bind(file));
+
+file.watch.call(
+  { showContent: () => console.log("call: hey sinon!") },
+  null,
+  __filename
+);
+file.watch.apply({ showContent: () => console.log("apply: hey sinon!") }, [
+  null,
+  __filename,
+]);
